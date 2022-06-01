ca98cc1cbdedc728ee67dd82e3ed56c2c0314cec
JISOON-Y <zsooon96@gmail.com>
2022-06-01 23:20:26 +0900

    W1ㅣhomework_v1

--- week-1/src/App.js
+++ week-1/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 // jsx 사용 규칙
 function App() {
-  const styles = {
-    border: "1px solid #eee",
-    padding: "30px",
-    color: "green",
-    width: "300px",
-    margin: "30px auto",
+  const boxStyle1 = {
+    border: "1px solid orange",
+    display: "flex",
+    width: "800px"
+  }
+  const boxStyle2 = {
+    border: "1px solid #000"
+
+  }
+  const boxStyle3 = {
+    border: "1px solid #eee"
   };
 
   return (
@@ -18,11 +23,24 @@ function App() {
     // 4. style 속성을 넣을 땐 {}안에 딕셔너리{key:value}로 넣어주기
     // 5. jsx에서 javascript 문법(map,삼항연산자)을 가져올 때는 {} 사용하기
     <div className="App">
-      <div style={styles}>
-        <h1 style={{ color: "orange", fontSize: "28px" }}>안녕하세요!</h1>
-        <hr />
-        <p style={{ textAlign: "left" }}>이름을 입력해주세요.</p>
-        <input type="text" />
+      <h1>오늘 할 일</h1>
+      <div style={boxStyle1}>
+        <div style={boxStyle2}>
+          <div style={boxStyle3}>
+            <p>고양이 밥주기</p>
+            <p>고양이 물, 사료 챙겨주기</p>
+            <button>완료</button>
+          </div>
+          <div style={boxStyle3}>
+            <p>장보기</p>
+            <p>토마토, 계란, 초코렛 사기</p>
+            <button>완료</button>
+          </div>
+        </div>
+        <div style={boxStyle2}>
+          <input type="text" />
+          <button>추가하기</button>
+        </div>
       </div>
     </div>
   );
