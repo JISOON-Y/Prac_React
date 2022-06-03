9fd906cabeed3e6c60d0b4387704c135de1097a4
JISOON-Y <zsooon96@gmail.com>
2022-06-03 14:55:39 +0900

    2-8ㅣstyled components

--- bucket_list/src/App.js
+++ bucket_list/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 // import './App.css';
-import './style.css';
+// import './style.css';
+import styled from 'styled-components';
 import React from 'react';
 import BucketList from './BucketList';
 // import { BucketList } from './BucketList';
@@ -19,13 +20,17 @@ class App extends React.Component {
     // console.log(this.state.list);
     return (
       <div className="App">
-        <div className='container'>
+        {/* style도 컴포넌트이기때문에 props 줄 수 있음 */}
+        <MyStyled>
+          <p>im here~!</p>
+        </MyStyled>
+        {/* <div className='container'>
           <h1>내 버킷 리스트</h1>
           <hr className='line' />
           {/* 컴포넌트 넣어주기 */}
-          {/* <컴포넌트 명 [props명]={넘겨줄 것(데이터)} */}
-          <BucketList list={this.state.list} />
-        </div>
+        {/* <컴포넌트 명 [props명]={넘겨줄 것(데이터)} */}
+        {/* <BucketList list={this.state.list} /> */}
+        {/* </div> */}
       </div>
     )
   }
@@ -40,5 +45,22 @@ class App extends React.Component {
 //     </div>
 //   );
 // }
+
+// 스타일 객체 만들기
+const MyStyled = styled.div`
+  width: 50vw;
+  height: 150px;
+  // background: slateblue;
+
+  // props 불러오기
+  // background-color: ${(props) => (props.bg_color)};
+  background-color: ${(props) => (props.bg_color ? "red" : "purple")};
+  p {
+    color: blue;
+  }
+  &:hover {
+    background-color: yellow;
+  }
+`;
 
 export default App;
