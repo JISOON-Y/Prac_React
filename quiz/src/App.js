2690fa9be4b019bca34577f71c4d34c60e109168
JISOON-Y <zsooon96@gmail.com>
2022-06-05 04:14:58 +0900

    W2ㅣhomework_style-styled components적용

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -7,58 +7,16 @@ function App() {
   const [name, setName] = React.useState("양지순")
 
   return (
-    <div className='App' style={{
-      // 컨텐츠의 최대 넓이 지정
-      maxWidth: "350px",
-      // 컨텐츠를 화면 가운데에 위치 지정
-      margin: "auto"
-    }}>
+    <AppStyle>
       <Start name={name} />
-    </div>
+    </AppStyle>
   )
 
 }
 
-
-const Container = styled.div`
-// background-color: #eee;
-width: 100vw;
-height: 100vh;
-display: flex;
-flex-direction: column;
+const AppStyle = styled.div`
+max-width: 350px;
+margin: auto;
 `;
-
-const TitleContainer = styled.div`
-// background-color: #ddd;
-margin: 200px auto 30px;
-`;
-
-const InputStyle = styled.input`
-background-color: #eee;
-width: 300px;
-height: 48px;
-margin: 20px auto;
-
-border: 0;
-border-radius: 100px;
-padding-left: 24px;
-font-size: 18px;
-font-weight: bold;
-`;
-
-const ButtonStyle = styled.button`
-background-color: slateblue;
-width: 140px;
-height: 48px;
-margin: 14px auto;
-
-border: 0;
-border-radius: 100px;
-color: white;
-font-size: 18px;
-font-weight: bold;
-box-shadow: 0px 2px 8px darkgray;
-`;
-
 
 export default App;
--- quiz/src/Start.js
+++ quiz/src/Start.js
@@ -1,61 +1,76 @@
 import React from 'react';
 import img from './dev_soon.png';
+import styled from 'styled-components';
 
 
 const Start = (props) => {
     console.log(props)
+    // 입력값 
 
     return (
-        <div style={{
-            display: "flex",
-            height: "100vh",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            padding: "30px",
-            boxSizing: "border-box"
-        }}>
-            <img src={img} style={{
-                width: "60vw",
-                margin: "16px"
-            }} />
+        <Container>
+            <ImgStyle src={img} />
 
-            <h1 style={{
-                fontSize: "1.5em",
-                lineHeight: "1.8",
-                textAlign: "center"
-            }}>나는 <span style={{
-                backgroundColor: "#ffe17c",
-                padding: "5px 10px",
-                borderRadius: "30px"
-            }}>{props.name}</span>에 대해<br /> 얼마나 알고 있을까?</h1>
+            <DescStyle>
+                나는 <PointStyle>{props.name}</PointStyle>에 대해
+                <br />얼마나 알고 있을까?
+            </DescStyle>
 
-            <input type="text" placeholder='내 이름' style={{
-                backgroundColor: "#eee",
-                width: "100%",
-                // height: "48px",
-                borderRadius: "100px",
-                border: "0",
-                fontSize: "18px",
-                fontWeight: "bold",
-                padding: "16px",
-            }} />
+            <InputStyle type="text" placeholder='내 이름' />
 
-            <button style={{
-                backgroundColor: "slateblue",
-                // width: "140px",
-                // height: "48px",
-                margin: "30px",
-                padding: "20px 48px",
-                border: "0",
-                borderRadius: "100px",
-                color: "#fff",
-                fontWeight: "bold",
-                boxShadow: "0px 2px 8px darkgray"
-            }}>시작하기</button>
-        </div >
+            <ButtonStyle>
+                시작하기
+            </ButtonStyle>
+        </Container >
     )
 }
 
+const Container = styled.div`
+display: flex;
+height: 100vh; 
+flex-direction: column;
+align-items: center;
+justify-content: center;
+padding: 30px;
+box-sizing: border-box;
+`;
+
+const ImgStyle = styled.img`
+width: 60vw;
+margin: 16px;
+`;
+
+const DescStyle = styled.h1`
+font-size: 1.5em;
+line-height: 1.8;
+text-align: center;
+`;
+
+const PointStyle = styled.span`
+background-color: #ffe17c;
+padding: 5px 10px;
+border-radius: 30px;
+`;
+
+const InputStyle = styled.input`
+background-color: #eee;
+width: 100%;
+border-radius: 100px;
+border: 0;
+font-size: 18px;
+font-weight: bold;
+padding: 16px;
+`;
+
+const ButtonStyle = styled.button`
+background-color: slateblue;
+margin: 30px;
+padding: 20px 48px;
+border: 0;
+border-radius: 100px;
+color: #fff;
+font-weight: bold;
+box-shadow: 0px 2px 8px darkgray;
+`;
 
 export default Start;
