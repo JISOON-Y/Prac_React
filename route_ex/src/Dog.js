6b2f537bef195e4770bf880fbc64f40bd19fdb0a
JISOON-Y <zsooon96@gmail.com>
2022-06-06 13:56:57 +0900

    3-4ㅣuseNavigate 훅을 활용한 페이지 이동

--- route_ex/src/Dog.js
+++ route_ex/src/Dog.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dog = (props) => {
+    // 페이지 이동을 위해 useNavigate 훅 사용 = Link를 사용할 수 없을 때
+    const navigate = useNavigate();
     return (
-        <div>댕댕이 화면입니다.</div>
+        <div onClick={() => {
+            navigate("/cat");
+        }}>댕댕이 화면입니다.</div>
     )
 }
 
