838f2b54b74e04d6a268304e4d2b3d01be58de3d
JISOON-Y <zsooon96@gmail.com>
2022-06-06 13:01:15 +0900

    3-4ㅣuseParams 훅을 활용한 동적 라우팅

--- route_ex/src/App.js
+++ route_ex/src/App.js
@@ -11,7 +11,8 @@ function App() {
       <Routes>
         {/* 자식 컴포넌트(Home)에 props 넘겨주기 */}
         <Route path="/" element={<Home data={"im data"} />} />
-        <Route path="/cat" element={<Cat />} />
+        {/* url 파라미터 사용 = 동적 라우팅 */}
+        <Route path="/cat/:cat_name" element={<Cat />} />
         <Route path="/dog" element={<Dog />} />
       </Routes>
     </div>
--- route_ex/src/Cat.js
+++ route_ex/src/Cat.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Cat = (props) => {
+    // useParams 훅 사용
+    const cat_name = useParams();
+    console.log(cat_name);
+
     return (
         <div>고양이 화면입니다.</div>
     )
