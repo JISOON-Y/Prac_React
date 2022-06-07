ba2fd05e59a8da4d7b7980eb21310bb79c38dd90
JISOON-Y <zsooon96@gmail.com>
2022-06-07 12:11:18 +0900

    3-9ㅣ모듈생성-리듀스 묶어주기-스토어 생성

--- route_ex/src/Dog.js
+++ route_ex/src/Dog.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const Dog = (props) => {
+    // react-router-dom v6에 따른 코드 수정 (useHistory -> useNavigate)
     // 페이지 이동을 위해 useNavigate 훅 사용 = Link를 사용할 수 없을 때
     const navigate = useNavigate();
     return (
